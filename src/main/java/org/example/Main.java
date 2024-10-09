8db448c7dd5dc8b36a68a122820b42d1ba00e00b
sanin <saninzajmovic@stu.ibu.edu.ba>
2024-10-09 15:40:11 +0200

    first commit/test

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for(int i=0; i<6; i++) {
+            System.out.println((String.valueOf(i)+" ").repeat(i));
+        }
+
+    }
+}
